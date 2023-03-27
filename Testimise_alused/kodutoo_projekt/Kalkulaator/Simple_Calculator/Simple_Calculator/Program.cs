cac4569761cb5f2b5504ef752e5e771f09f4dde4
Kenneth Muuli <kenneth.muuli@gmail.com>
2023-03-27 21:58:19 +0300

    Lõpetasin kodutöö projekti, testiraportite kirjutamine ja vigade parandamine

--- Testimise_alused/kodutoo_projekt/Kalkulaator/Simple_Calculator/Simple_Calculator/Program.cs
+++ Testimise_alused/kodutoo_projekt/Kalkulaator/Simple_Calculator/Simple_Calculator/Program.cs
@@ -1,5 +1,13 @@
 ﻿using System.Text;
 
+/* Leitud vead */
+//  1. Tehted üksteise otsas ühel real.
+//  2. Juhise teine rida vigane.
+// „Type in the numbers to be calculated and use +, -, *, / operators in between to specify the operation you want to complete.“
+//  3. Tulemust on näiliselt võimalik muuta otse (juurde kirjutades)
+//  4. “,” (koma) sisend ei tööta??
+//  5. Helpi avamine kaotab ära konsoolist viimase avaldise
+
 class Calculator
 {
     public static double? a;
@@ -41,10 +49,12 @@ class Calculator
         {
             Console.Clear();
             checkedHelp = false;
+            DisplayCurrentExpression();
         }
 
+        
         switch (pressedKey.Key)
-        {
+        {   
             case ConsoleKey.D1:
             case ConsoleKey.D2:
             case ConsoleKey.D3:
@@ -65,35 +75,40 @@ class Calculator
             case ConsoleKey.NumPad8:
             case ConsoleKey.NumPad9:
             case ConsoleKey.NumPad0:
-            case ConsoleKey.Decimal:
                 {
-                    Console.Write(pressedKey.KeyChar);
-                    stringBuilder.Append(pressedKey.KeyChar);
+                    if (a == null || _operator != null && b == null)
+                    {
+                        Console.Write(pressedKey.KeyChar);
+                        stringBuilder.Append(pressedKey.KeyChar);
+                    }
                     break;
                 }
+            case ConsoleKey.Decimal:
+                {  
+                    if (a == null || _operator != null && b== null)
+                    {
+                        //Asendame, numpadi "decimal" klahvi sisendi punktiga, et vältida klaviatuuri keele spetsiifiliseid erisusi, mis võivad sinna tegelikult vastasel juhul koma ette tuua.
+                        Console.Write(".");
+                        stringBuilder.Append(".");
+                    }
+                    break;
+                }
+            //matemaatiliste operaatori klahvide vajutamise loogika blokk
             case ConsoleKey.Divide:
             case ConsoleKey.Multiply:
             case ConsoleKey.Subtract:
             case ConsoleKey.Add:
                 {
-
                     if (a == null && stringBuilder.Length > 0)
                     {
                         a = Convert.ToDouble(Convert.ToString(stringBuilder));
                         stringBuilder.Clear();
 
-                        stringBuilder.Append(pressedKey.KeyChar);
-                        _operator = Convert.ToString(stringBuilder);
-                        Console.Write(" " + pressedKey.KeyChar + " ");
-                        stringBuilder.Clear();
+                        AppendAndWrite(pressedKey);
                     }
                     else if (_operator == null)
                     {
-
-                        stringBuilder.Append(pressedKey.KeyChar);
-                        _operator = Convert.ToString(stringBuilder);
-                        Console.Write(" " + pressedKey.KeyChar + " ");
-                        stringBuilder.Clear();
+                        AppendAndWrite(pressedKey);
                     }
                     else if (b == null && stringBuilder.Length > 0)
                     {
@@ -103,18 +118,11 @@ class Calculator
                         if (a != null & b != null & _operator != null)
                         {
                             calculation.Calculate(Convert.ToDouble(a), Convert.ToDouble(b), _operator);
-
-                            stringBuilder.Append(pressedKey.KeyChar);
-                            _operator = Convert.ToString(stringBuilder);
-                            Console.Write(" " + pressedKey.KeyChar + " ");
-                            stringBuilder.Clear();
+                            AppendAndWrite(pressedKey);
                         }
                         else
                         {
-                            stringBuilder.Append(pressedKey.KeyChar);
-                            _operator = Convert.ToString(stringBuilder);
-                            Console.Write(" " + pressedKey.KeyChar + " ");
-                            stringBuilder.Clear();
+                            AppendAndWrite(pressedKey);
                         }
                     }
                     break;
@@ -140,7 +148,7 @@ class Calculator
                         else break;
                     }
 
-                    // --- To see captured values after pressing enter key
+                    //Testimise tarbeks, kirjutab välja salvestatud väärtuste hetke seisu
                     //Console.WriteLine(" 'Values check: A = " + a + " | B = " + b + " | Operator = " + _operator + "'");
 
                     calculation.Calculate(Convert.ToDouble(a), Convert.ToDouble(b), _operator);
@@ -160,42 +168,81 @@ class Calculator
 
             default: break;
         }
+        
+
     }
 
     public void Calculate(double value1, double value2, string function)
     {
-        //Console.Clear();
-
         switch (function)
         {
             case "+":
                 {
                     value = a + b;
-                    //Console.Write(a + " + " + b + " = " + value);
-                    Console.Write(" = " + value);
+                    Console.Write(" = " + value + "\n" + value);
                     a = value; b = null; _operator = null;
                     break;
                 }
             case "-":
                 value = a - b;
-                Console.Write(" = " + value);
+                Console.Write(" = " + value + "\n" + value);
                 a = value; b = null; _operator = null;
                 break;
             case "*":
                 {
                     value = a * b;
-                    Console.Write(" = " + value);
+                    Console.Write(" = " + value + "\n" + value);
                     a = value; b = null; _operator = null;
                     break;
                 }
             case "/":
                 {
                     value = a / b;
-                    Console.Write(" = " + value);
+                    Console.Write(" = " + value + "\n" + value);
                     a = value; b = null; _operator = null;
                     break;
                 }
             default: break;
+        }
+    }
+
+    public static void AppendAndWrite(ConsoleKeyInfo pressedKey)
+    {
+        stringBuilder.Append(pressedKey.KeyChar);
+        _operator = Convert.ToString(stringBuilder);
+        Console.Write(" " + pressedKey.KeyChar + " ");
+        stringBuilder.Clear();
+    }
+
+    //kuva viimast avaldist hetke seisus, s.t. nii palju kui kasutaja on seni sisestanud
+    public static void DisplayCurrentExpression()
+    {
+
+        //kontroll, et stringBuilder poleks parasjagu null väärtusega, vältimaks null ref excep.
+        if(stringBuilder != null) 
+        {
+
+            //kui esimene avaldise väärtus on kirjutatud, kuid pole veel a avalise väärtusena salvestatud siis salvesta see, et seda saaks üksinda välja kuvada
+            if (a == null && stringBuilder.Length > 0)
+            {
+                a = Convert.ToDouble(Convert.ToString(stringBuilder));
+                stringBuilder.Clear();
+            }
+        }
+
+        if (a != null)
+        {
+            Console.Write(a);
+
+            if (_operator != null)
+            {
+                Console.Write(" " + _operator + " ");
+
+                if (b != null)
+                {
+                    Console.Write(" " + b);
+                }
+            }
         }
     }
 
@@ -208,17 +255,21 @@ class Calculator
         b = null;
         _operator = null;
         value = null;
-
         justReset = true;
     }
 
     public void DisplayHelp()
     {
+        Console.Clear();
+
         Console.WriteLine();
-        Console.WriteLine("This is a simple calculator able to do adding, subtraction, multiplying and dividing operations.");
-        Console.WriteLine("Type in the numbers to be calculated and use +, -, *, / in between respectively.");
+        Console.WriteLine("This is a simple calculator able to do adding, subtraction, multiplying and dividing operations. It is designed to be used on the numpad of the keyboard.");
+        Console.WriteLine("Type in the numbers to be calculated and use +, -, *, / operators in between to specify the operation you want to complete.");
+        Console.WriteLine("Press ENTER to calculate the value of your expression or just keep on writing your calculations (the calculations will be displayed automatically if you have two variables and an operator entered).");
         Console.WriteLine("Press R to reset the calculator, ESCAPE to close the program or H to display this message again.");
-        Console.WriteLine("Press ENTER to calculate the value of your expression or just keep on writing your calculations.");
+
+        DisplayCurrentExpression();
+
         checkedHelp = true;
     }
 }
